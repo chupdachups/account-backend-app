3583facace4cd298e8f0b6985b793db2ed965069
chupdachups <chupdachups@gmail.com>
2022-05-03 17:14:40 +0900

    log 추가

--- src/main/java/com/msa/account/controller/AccountController.java
+++ src/main/java/com/msa/account/controller/AccountController.java
@@ -1,17 +1,13 @@
 package com.msa.account.controller;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,15 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.msa.account.dto.AccountDto;
-import com.msa.account.entity.Account;
 import com.msa.account.service.AccountService;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 @RestController
 @RequestMapping("accounts")
@@ -41,6 +34,7 @@ public class AccountController {
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Mono<String> signUp(@RequestBody @Valid final AccountDto.SignUpReq dto) {
+    	log.info("account-service sign up call");;
     	return accountService.create(dto);
     }
 
